0ebe466c366ae8f95fef58cc694bd8bc49be2537
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-01 10:30:49 -0800

    Count from 40 to 1 by Fives completed

--- RangeFunction.py
+++ RangeFunction.py
@@ -5,7 +5,8 @@ for i in range(40, 0, -1):
 
 """Repeat the exercise but count by 5's"""
 
-
+for i in range(40, 0, -5):
+    print(i)
 
 """Write a program that will count print all the multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
