512f6220659c14af5c0456fe6980e48b2e5ef722
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-01 11:41:03 -0800

    Encrypt Problem 1 completed

--- Encrypt.py
+++ Encrypt.py
@@ -3,11 +3,35 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
+"""
 
+def RemoveVowels(NormText):
+    CipherText = ''
+    for ch in NormText:
+        if ch == 'a':
+            CipherText += ''
+        elif ch == 'e':
+            CipherText += ''
+        elif ch == 'i':
+            CipherText += ''
+        elif ch == 'o':
+            CipherText += ''
+        elif ch == 'u':
+            CipherText += ''
+        elif ch == 'A':
+            CipherText += ''
+        elif ch == 'E':
+            CipherText += ''
+        elif ch == 'I':
+            CipherText += ''
+        elif ch == 'O':
+            CipherText += ''
+        elif ch == 'U':
+            CipherText += ''
+        else:
+            CipherText += ch
+    return CipherText
 
-
-
-
+print(RemoveVowels('Computer Science Makes the World go round but it doesn\'t make the world round itself!'))
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
