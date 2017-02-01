8b4f3746e244d72494883e58b7941c093d5c60a9
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-01 10:13:19 -0800

    DrawRectangle function completed

--- MyFile.py
+++ MyFile.py
@@ -4,4 +4,22 @@ AddTen Function
 def AddTen(n):
     return n + 10
 
-print(AddTen(13))
+x = 13
+print(str(x) +" + 10 =" , str(AddTen(x)))
+
+
+"""
+DrawRectangle Function
+"""
+def DrawRectangle(AnyTurtle, l, w):
+    import turtle
+    AnyTurtle = turtle.Turtle()
+    wn = turtle.Screen()
+    for i in range(2):
+        AnyTurtle.fd(l)
+        AnyTurtle.left(90)
+        AnyTurtle.fd(w)
+        AnyTurtle.left(90)
+    wn.exitonclick()
+
+DrawRectangle("Bob", 150, 120)
