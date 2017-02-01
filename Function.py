598a763e08a47aea5f9142a03537ff2723743919
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-01 10:22:17 -0800

    Triple function completed

--- Function.py
+++ Function.py
@@ -1,9 +1,11 @@
 """Define a function that will take a parameter, n, and triple it and return
 the result"""
 
+def Triple(n):
+    return n*3
 
-
-
+x = 12
+print(str(x) + ' multiplied by 3 is', (Triple(x)))
 
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
