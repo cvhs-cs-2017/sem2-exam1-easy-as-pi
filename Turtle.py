f2ca832ee495ea4086defe40c380bf2be2ce6afa
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-01 11:18:31 -0800

    Turtle Problem 1 completed

--- Turtle.py
+++ Turtle.py
@@ -1,7 +1,28 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
 
-
-
+import turtle
+hi = turtle.Turtle()
+wn = turtle.Screen()
+for i in range(4):
+    hi.fd(50)
+    hi.left(90)
+hi.goto(20,20)
+for i in range(4):
+    hi.fd(50)
+    hi.left(90)
+hi.pu()
+hi.goto(50,0)
+hi.pd()
+hi.goto(70,20)
+hi.pu()
+hi.goto(50,50)
+hi.pd()
+hi.goto(70,70)
+hi.pu()
+hi.goto(0,50)
+hi.pd()
+hi.goto(20,70)
+wn.exitonclick()
 
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
