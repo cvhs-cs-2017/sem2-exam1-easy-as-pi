dd06b4c2ebde9d8e805acec36cbef42126c6107f
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-02-03 10:37:43 -0800

    ExtraCreditOptional completed!

--- ExtraCredit.optional.py
+++ ExtraCredit.optional.py
@@ -2,10 +2,39 @@
 
 example:  10011 would return (16+0+0+2+1 = 19)"""
 
-
-
-
-
-
+def BinaryToNum(num):
+    length = len(num)
+    tracker = 0
+    ind = 0
+    for i in num:
+        new = (int(num[ind])*(2**(length-(ind+1))))
+        tracker += new
+        ind += 1
+    return tracker
+print(BinaryToNum("10011"))
 
 """Write a program that would take a real number value and convert it into BINARY"""
+"""
+def NumToBinary(num2):
+    import math
+    binary = ''
+    while num2 != 0:
+        Floorlog2 = math.floor(math.log(int(num2),2))
+
+    return floor
+print(NumToBinary('9'))
+"""
+
+def NumToBinary(num2):
+    import math
+    Floorlog = math.floor(math.log(int(num2),2))
+    binary = ''
+    while num2 != 0:
+        binarynum = math.floor((num2/(2**Floorlog)))
+        binary += str(binarynum)
+        if binarynum != 0:
+            num2 = num2 - (2**Floorlog)
+        Floorlog -= 1
+    return binary
+    #return binarynum
+print(NumToBinary(9))
